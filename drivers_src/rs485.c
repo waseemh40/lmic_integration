5022928ee4a3506ac8ef3e662ee62e3a3d061944
Waseem Hassan <waseemh40@gmail.com>
2018-10-25 15:58:50 +0200

    Actually while 1 is removed in this commit not is last/

--- drivers_src/rs485.c
+++ drivers_src/rs485.c
@@ -129,17 +129,17 @@ void RS485_ISR(){
 	LEUART_IntClear(RS485_UART, LEUART_IF_TXBL);
 	LEUART_IntClear(RS485_UART, LEUART_IF_RXDATAV);
 	 if (RS485_UART->STATUS & LEUART_STATUS_TXBL){
-		if(!fifo_tbr_is_empty(fifo_tbr_tx_cmd)){
+		while(!fifo_tbr_is_empty(fifo_tbr_tx_cmd)){
 			isr_rx_tx_char=fifo_tbr_remove(fifo_tbr_tx_cmd);
 			LEUART_FreezeEnable(RS485_UART,true);
 			LEUART_Tx(RS485_UART,(uint8_t)isr_rx_tx_char );
 			LEUART_FreezeEnable(RS485_UART,false);
-			delay_ms(1);
+			delay_ms(0);
 		}
-	  else{
+	  //else{
 		  	LEUART_IntDisable(RS485_UART,LEUART_IF_TXBL);
 		  	rs485_rx_mode();
-	  	}
+	  //	}
 	  }
 	 if (RS485_UART->STATUS & LEUART_STATUS_RXDATAV){
 		 LEUART_FreezeEnable(RS485_UART,true);
